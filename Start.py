b6a35b07726674132c84c4474a9382600225ad42
Chi-Wen "Toby" Teng <52705034+toby0622@users.noreply.github.com>
2023-07-13 17:29:37 +0800

    Problem Unsolved

--- Start.py
+++ Start.py
@@ -2,6 +2,7 @@ from OCR import *
 from Search import *
 from Export import *
 
+
 if __name__ == "__main__":
     image_path = "Book Cover Dataset/"
     ocr_txt_output = "Cover TXT/"
@@ -12,9 +13,11 @@ if __name__ == "__main__":
     data = []
 
     # book cover ocr, total case 1544
-    for i in range(0, 5):
+    for i in range(0, 1):
         textfile = image_ocr_match(image_path, i)
-        data.append(textfile)
+
+        for j in textfile:
+            data.append(j)
 
     txt_export(data, ocr_txt_output)
 
